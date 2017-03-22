34e6ac76854386d8f8b4b5d5addb80817c36a169
wangchangfei <1273345501@qq.com>
2017-03-22 11:54:03 +0800

    初始化

---
+++ FirstRespository/FirstRespository/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FirstRespository
+//
+//  Created by mac on 17/3/22.
+//  Copyright © 2017年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property(nonatomic,strong)NSString *test;
+@end
+
---
+++ FirstRespository/FirstRespository/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  FirstRespository
+//
+//  Created by mac on 17/3/22.
+//  Copyright © 2017年 mac. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.view.backgroundColor  =[UIColor redColor];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
